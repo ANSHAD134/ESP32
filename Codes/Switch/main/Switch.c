0646589fd4a09ba7ce83ef9ec6aea1f63fcffe21
Anshad ck <anshadck1020@gmail.com>
2026-03-07 14:48:25 +0530

    Added ESP32_Button_Program

--- Codes/Switch/main/Switch.c
+++ Codes/Switch/main/Switch.c
@@ -3,15 +3,15 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#define SWITCH_PIN     0
+#define SWITCH_PIN     5
 #define LED_PIN        2
 
 void app_main(void)
 {
-    // Configure GPIO Pin 0 as an Input
-    gpio_reset_pin(SWITCH_PIN);                      // Reset GPIO pin 0
-    gpio_set_direction(SWITCH_PIN, GPIO_MODE_INPUT); // Set GPIO pin 0 as input
-    gpio_pullup_en(SWITCH_PIN);                      // Enable pull-up resistor for GPIO pin 0
+    // Configure GPIO Pin 5 as an Input
+    gpio_reset_pin(SWITCH_PIN);                      // Reset GPIO pin 5
+    gpio_set_direction(SWITCH_PIN, GPIO_MODE_INPUT); // Set GPIO pin 5 as input
+    gpio_pullup_en(SWITCH_PIN);                      // Enable pull-up resistor for GPIO pin 5
 
     // Configure GPIO Pin 2 as an output
     gpio_reset_pin(LED_PIN);                         // Reset GPIO pin 2
@@ -22,14 +22,14 @@ void app_main(void)
         // Check the state of the switch
         if (gpio_get_level(SWITCH_PIN) == 0)
         {
-            gpio_set_level(LED_PIN, 1);  // Turn on the LED
+            gpio_set_level(LED_PIN, 1);             // Turn on the LED
         }
         else
         {
-            gpio_set_level(LED_PIN, 0);  // Turn off the LED
+            gpio_set_level(LED_PIN, 0);             // Turn off the LED
         }
 
-        vTaskDelay(100 / portTICK_PERIOD_MS);  // Delay for 100ms
+        vTaskDelay(100 / portTICK_PERIOD_MS);      // Delay for 100ms
     }
     
 
