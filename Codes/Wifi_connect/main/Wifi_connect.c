bdd6ea2246750d34b17e83e13d5f73de1881c531
Anshad ck <anshadck1020@gmail.com>
2026-04-17 19:55:01 +0530

    Edited Wifi_connect Program

--- Codes/Wifi_connect/main/Wifi_connect.c
+++ Codes/Wifi_connect/main/Wifi_connect.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "nvs_flash.h"
 
 void app_main(void)
 {
-
+    nvs_flash_init();
+    esp_netif_init();
 }
