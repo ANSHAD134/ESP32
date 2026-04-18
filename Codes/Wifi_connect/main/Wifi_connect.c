453c885be481c10a76fdbf9dc249d0f94136eecd
Anshad ck <anshadck1020@gmail.com>
2026-04-18 20:37:22 +0530

    Edited Wifi_connect Program

--- Codes/Wifi_connect/main/Wifi_connect.c
+++ Codes/Wifi_connect/main/Wifi_connect.c
@@ -9,7 +9,7 @@
 #define SSID        "Airtel_azha_2428"
 #define PASSWORD    "air74947"
 
-static void Wifi_event_handler(Void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) 
     {
@@ -19,13 +19,13 @@ static void Wifi_event_handler(Void* arg, esp_event_base_t event_base, int32_t e
     else if(event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
     {
         printf("Connection successful\n");
-        ip_event_got_ip_t* event = (ip_event_got_ip*) event_data;
-        printf("Got IP: " IPSTR "\n", IP2STR(&event->ip.info.ip));
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        printf("Got IP: " IPSTR "\n",IP2STR(&event->ip_info.ip));
     }
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
     {
         printf("Wifi disconnected\n");
-        wifi_event_sta_disconnected_t* event = (wifi_event_sta_disconnected*) event_data;
+        wifi_event_sta_disconnected_t* event = (wifi_event_sta_disconnected_t*) event_data;
         printf("Reason: %d\n", event->reason);
         esp_wifi_connect();
     }
@@ -41,8 +41,8 @@ void app_main(void)
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&cfg);
 
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, Wifi_event_handler, NULL);
-    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, Wifi_event_handler, NULL);
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
 
     wifi_config_t config = {
     .sta = {
